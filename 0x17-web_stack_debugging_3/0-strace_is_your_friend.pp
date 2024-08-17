2125a1af4d2500119dfafa1360e90ab06c0ecff3
temitope <taymeedouglas07@gmail.com>
2024-08-17 05:50:28 +0000

    stance is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,7 @@
-# A puppet script to replace a line in a file on a server
+# A puppet that fixes a wordpress site changing the error from 500 to 200
+# editing the mistyped .phpp to php in the settings.php file
 
-$file_to_edit = '/var/www/html/wp-settings.php'
-
-#replace line containing "phpp" with "php"
-
-exec { 'replace_line':
-  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
-  path    => ['/bin','/usr/bin']
+exec { 'fix-wordpress-server-erroe';
+    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    path => '/usr/bin/:/bin/',
 }
